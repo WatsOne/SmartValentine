8c70b5d57eac079ac2958a3f4ce2471910db8db2
WatsOne <thewato@gmail.com>
2016-05-31 20:58:49 +0500

    music test

--- app/src/main/java/ru/alexkulikov/smartvalentine/MainActivity.java
+++ app/src/main/java/ru/alexkulikov/smartvalentine/MainActivity.java
@@ -1,11 +1,21 @@
 package ru.alexkulikov.smartvalentine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +29,44 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                VKSdk.login(MainActivity.this, "photo");
+                VKSdk.login(MainActivity.this, "audio");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                sendRequest();
+            }
+
+            @Override
+            public void onError(VKError error) {
+
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void sendRequest() {
+        VKRequest request = VKApi.audio().get(VKParameters.from(VKApiConst.OWNER_ID, "17764970"));
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+
+            }
+            @Override
+            public void onError(VKError error) {
+
+
+            }
+            @Override
+            public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
+
+
             }
         });
     }
